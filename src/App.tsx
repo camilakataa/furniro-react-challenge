1318318aefeec9cb734bedd603a2d992ad7bd908
Camila Katayama <camilakatayamap@gmail.com>
2024-07-23 17:46:53 -0300

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
+import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,7 +13,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Shop' element={<Shop />} />
+        <Route path='/About' element={<About />}/>
         <Route path='/Contact' element={<Contact />} />
+        <Route path='/SingleProduct' element={<SingleProduct />}/>
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/Checkout' element={<Checkout />} />
       </Routes>
     </>
   )
