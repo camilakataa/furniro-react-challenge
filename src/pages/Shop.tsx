a6de972a936e667c975f1db27f79d2a4049fec18
Camila Katayama <camilakatayamap@gmail.com>
2024-07-25 15:40:57 -0300

    added menu filter

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -2,12 +2,14 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
 import Benefits from "../components/Benefits";
+import FilterMenu from "../components/FilterMenu";
 
 const Shop = () => {
   return (
     <>
         <Header />
         <Banner namePage="Shop" />
+        <FilterMenu />
         <Benefits />
         <Footer />
     </>
