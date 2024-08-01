a803b6cc138189e41c8eeb5ef7374bec9ed3b155
Camila Katayama <camilakatayamap@gmail.com>
2024-08-01 12:10:28 -0300

    added pages in shop section

--- src/components/BannerDiscover.tsx
+++ src/components/BannerDiscover.tsx
@@ -1,4 +1,5 @@
 import BannerDiscoveryImg from '../assets/bg-banner-home.png'
+import { Link } from 'react-router-dom'
 
 const BannerDiscover = () => {
   return (
@@ -10,9 +11,11 @@ const BannerDiscover = () => {
                 <p className='font-semibold text-gray-600'>New Arrival</p>
                 <h2 className='text-yellow-dark font-bold text-[28px] pb-4 lg:text-[52px] lg:pb-2'>Discover Our New Collection</h2>
                 <p className='text-sm text-gray-500 pb-14'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                <Link to="/Shop" >
                 <button className='bg-yellow-dark text-white w-[200px] h-[70px] font-bold lg:w-[222px] cursor-pointer'>
                     BUY NOW
                 </button>
+                </Link>
             </div>
         </div>
     </>
--- src/components/ShopSection.tsx
+++ src/components/ShopSection.tsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react";
 import CardProduct from "./CardProduct";
 import { IProducts } from "../types/CartTypes";
 import axios from "axios";
-import FilterIcon from '../assets/icon-filter.png';
-import FilterIcon2 from '../assets/icon-filter2.png';
-import FilterIcon3 from '../assets/icon-filter3.png';
+import FilterIcon from "../assets/icon-filter.png";
+import FilterIcon2 from "../assets/icon-filter2.png";
+import FilterIcon3 from "../assets/icon-filter3.png";
 
 const ShopSection = () => {
-  const [products, setProducts] = useState<IProducts[]>([])
-  const [numberShowItens, setNumberShowItens] = useState<number>(16)
-  
+  const [products, setProducts] = useState<IProducts[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 16;
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/products/`)
@@ -19,40 +21,64 @@ const ShopSection = () => {
       .catch(function (error) {
         console.log(error);
       });
-
   }, []);
 
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
   return (
     <div>
       <div className="w-full flex flex-col gap-8 bg-beige-light px-10 py-6 items-center lg:flex-row lg:justify-between xl:px-16">
-        <div className='flex gap-8 items-center'>
-            <img className='h-[24px]' src={FilterIcon} />
-            <p>Filter</p>
-            <img className='h-[24px]' src={FilterIcon2} />
-            <img className='h-[24px]' src={FilterIcon3} />
-            <p className='border-solid border border-y-0 border-r-0 pl-6 border-gray-400'>Showing 1-16 of {products.length} results</p>
+        <div className="flex gap-8 items-center">
+          <img className="h-[24px]" src={FilterIcon} />
+          <p>Filter</p>
+          <img className="h-[24px]" src={FilterIcon2} />
+          <img className="h-[24px]" src={FilterIcon3} />
+          <p className="border-solid border border-y-0 border-r-0 pl-6 border-gray-400">
+            Showing {startIndex + 1}-
+            {products.length > endIndex ? endIndex : products.length} of{" "}
+            {products.length} results
+          </p>
         </div>
-        <div className='flex items-center gap-8'>
-            <p>Show</p>
-            <p className='bg-white py-3 px-6 text-gray-400 text-light'>{numberShowItens}</p>
-            <p>Sort by</p>
-            <p className='bg-white py-3 px-8 text-gray-400 text-light'>Default</p>
+        <div className="flex items-center gap-8">
+          <p>Show</p>
+          <p className="bg-white py-3 px-6 text-gray-400 text-light">
+            {itemsPerPage}
+          </p>
+          <p>Sort by</p>
+          <p className="bg-white py-3 px-8 text-gray-400 text-light">Default</p>
         </div>
-    </div>
+      </div>
       <ul className="flex flex-wrap justify-center items-center py-14 px-16 gap-6">
-        { products?.slice(0, numberShowItens).map((item: IProducts) => {
+        {products?.slice(startIndex, endIndex).map((item: IProducts) => {
           return (
             <li key={item.id}>
               <CardProduct id={item.id} />
             </li>
-        )
+          );
         })}
       </ul>
-        <div>
+      <div className="flex justify-center">
+        <div className="flex w-[360px] justify-evenly items-center mb-20">
+          {[...Array(totalPages).keys()].map((_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              className={`bg-beige-light w-[60px] h-[60px] rounded-lg font-medium text-[20px] ${
+                currentPage === index + 1 ? "bg-yellow-dark text-white" : ""
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShopSection
+export default ShopSection;
